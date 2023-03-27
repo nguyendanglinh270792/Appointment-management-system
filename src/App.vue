d814bc8315bd9ee895182aa07d99d3c55fb29829
linh nguyen <nguyendanglinh270792l@gmail.com>
2023-03-27 15:21:44 +0700

    Initial Project

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</template>
+
+<script>
+export default {
+  name: 'TestApp',
+
+  data() {
+    return {
+      
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+    
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
